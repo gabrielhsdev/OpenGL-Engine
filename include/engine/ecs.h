90a79aeb7a39269ce52007d8bf2bda905d9c3420
gabrielhsdev <henriquegabrielhs21@gmail.com>
2025-08-19 17:41:20 -0300

    ECS

---
+++ include/engine/ecs.h
@@ -0,0 +1,235 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <unordered_map>
+#include <iostream>
+#include <glad/glad.h>
+#include <entt/entt.hpp>
+#include "engine/Shader.h"
+
+// --- Components ---
+struct Transform {
+    glm::vec3 position{0.0f};
+    glm::vec3 rotation{0.0f}; // Euler angles (degrees)
+    glm::vec3 scale{1.0f};
+};
+
+struct Camera {
+    glm::vec3 position{0.0f, 0.0f, 3.0f};
+    glm::vec3 front{0.0f, 0.0f, -1.0f};
+    glm::vec3 up{0.0f, 1.0f, 0.0f};
+    glm::vec3 right{1.0f, 0.0f, 0.0f};
+    glm::vec3 worldUp{0.0f, 1.0f, 0.0f};
+    float yaw{-90.0f};
+    float pitch{0.0f};
+    float fov{45.0f};
+};
+
+struct CameraController {
+    float movementSpeed{2.5f};
+    float mouseSensitivity{0.1f};
+};
+
+struct Input {
+    std::unordered_map<int, bool> keys;
+    float mouseX{0.0f}, mouseY{0.0f};
+    float deltaX{0.0f}, deltaY{0.0f};
+    bool firstMouse{true};
+};
+
+// This is for simple mesh rendering
+struct MeshRenderer {
+    unsigned int VAO{0};
+    unsigned int VBO{0};
+    unsigned int vertexCount{0};
+    unsigned int texture1{0};
+    unsigned int texture2{0};
+};
+
+// TODO: This is for more complex models with multiple meshes, add assimp. Dont remove this for now
+struct ModelMesh {
+    unsigned int VAO{0};
+    unsigned int VBO{0};
+    unsigned int EBO{0};
+    unsigned int indexCount{0};
+    unsigned int texture1{0};
+    unsigned int texture2{0};
+};
+
+// TODO: This is for more complex models with multiple meshes, add assimp. Dont remove this for now
+struct ModelRenderer {
+    std::vector<ModelMesh> meshes;
+};
+
+// --- Helper Enums ---
+enum class CameraMovement : std::uint8_t { FORWARD, BACKWARD, LEFT, RIGHT };
+
+// --- Base System Class ---
+class BaseSystem {
+  public:
+    virtual ~BaseSystem() = default;
+    virtual void update(entt::registry& registry, float deltaTime) = 0;
+};
+
+// --- Camera System ---
+class CameraSystem {
+  public:
+    static void updateCameraVectors(Camera& cam) {
+        glm::vec3 front;
+        front.x = cos(glm::radians(cam.yaw)) * cos(glm::radians(cam.pitch));
+        front.y = sin(glm::radians(cam.pitch));
+        front.z = sin(glm::radians(cam.yaw)) * cos(glm::radians(cam.pitch));
+        cam.front = glm::normalize(front);
+        cam.right = glm::normalize(glm::cross(cam.front, cam.worldUp));
+        cam.up = glm::normalize(glm::cross(cam.right, cam.front));
+    }
+
+    static glm::mat4 getViewMatrix(const Camera& cam) {
+        return glm::lookAt(cam.position, cam.position + cam.front, cam.up);
+    }
+
+    static void processKeyboard(Camera& cam,
+                                const CameraController& ctrl,
+                                CameraMovement dir,
+                                float deltaTime) {
+        float velocity = ctrl.movementSpeed * deltaTime;
+        switch (dir) {
+        case CameraMovement::FORWARD:
+            cam.position += cam.front * velocity;
+            break;
+        case CameraMovement::BACKWARD:
+            cam.position -= cam.front * velocity;
+            break;
+        case CameraMovement::LEFT:
+            cam.position -= cam.right * velocity;
+            break;
+        case CameraMovement::RIGHT:
+            cam.position += cam.right * velocity;
+            break;
+        }
+        updateCameraVectors(cam);
+    }
+
+    static void processMouse(Camera& cam,
+                             const CameraController& ctrl,
+                             float xoffset,
+                             float yoffset,
+                             bool constrainPitch = true) {
+        xoffset *= ctrl.mouseSensitivity;
+        yoffset *= ctrl.mouseSensitivity;
+
+        cam.yaw += xoffset;
+        cam.pitch += yoffset;
+
+        if (constrainPitch)
+            cam.pitch = std::clamp(cam.pitch, -89.0f, 89.0f);
+
+        updateCameraVectors(cam);
+    }
+};
+
+// --- Input System ---
+class InputSystem {
+  public:
+    static void handleMouse(Input& input, double xpos, double ypos) {
+        if (input.firstMouse) {
+            input.mouseX = xpos;
+            input.mouseY = ypos;
+            input.firstMouse = false;
+        }
+
+        input.deltaX = xpos - input.mouseX;
+        input.deltaY = ypos - input.mouseY;
+        input.mouseX = xpos;
+        input.mouseY = ypos;
+    }
+
+    static void resetDeltas(Input& input) {
+        input.deltaX = 0.0f;
+        input.deltaY = 0.0f;
+    }
+};
+
+// --- Mesh System ---
+class MeshSystem {
+  public:
+    static MeshRenderer createCube(float* vertices,
+                                   size_t vertSize,
+                                   unsigned int texture1 = 0,
+                                   unsigned int texture2 = 0) {
+        MeshRenderer mesh;
+        glGenVertexArrays(1, &mesh.VAO);
+        glGenBuffers(1, &mesh.VBO);
+
+        glBindVertexArray(mesh.VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, mesh.VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertSize, vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(
+            1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(
+            2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+        glBindVertexArray(0);
+
+        mesh.vertexCount = static_cast<unsigned int>(vertSize / (8 * sizeof(float)));
+        mesh.texture1 = texture1;
+        mesh.texture2 = texture2;
+        return mesh;
+    }
+};
+
+// --- Rendering System ---
+class RenderingSystem {
+  public:
+    static void renderEntity(const Transform& tf, const MeshRenderer& mesh, Shader& shader) {
+        if (mesh.texture1 != 0u) {
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, mesh.texture1);
+        }
+        if (mesh.texture2 != 0u) {
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, mesh.texture2);
+        }
+
+        glBindVertexArray(mesh.VAO);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, tf.position);
+        model = glm::rotate(model, glm::radians(tf.rotation.x), glm::vec3(1, 0, 0));
+        model = glm::rotate(model, glm::radians(tf.rotation.y), glm::vec3(0, 1, 0));
+        model = glm::rotate(model, glm::radians(tf.rotation.z), glm::vec3(0, 0, 1));
+        model = glm::scale(model, tf.scale);
+
+        shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, mesh.vertexCount);
+
+        glBindVertexArray(0);
+    }
+
+    static void renderScene(entt::registry& registry,
+                            Shader& shader,
+                            const Camera& cam,
+                            int width,
+                            int height) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 view = CameraSystem::getViewMatrix(cam);
+        glm::mat4 projection =
+            glm::perspective(glm::radians(cam.fov), float(width) / height, 0.1f, 100.0f);
+
+        shader.use();
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
+
+        auto viewMesh = registry.view<Transform, MeshRenderer>();
+        viewMesh.each([&](auto& tf, auto& mesh) { renderEntity(tf, mesh, shader); });
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,12 @@
 #include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-// Engine
 #include <engine/Shader.h>
-#include <engine/InputManager.h>
-#include <engine/Camera.h>
-#include <engine/CameraController.h>
+#include <engine/ecs.h> // All ECS logic and Shader
 #include <engine/stb_image.h>
 
 // clang-format off
-float RECTANGLE_VERTICES[] = {
-    // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-};
-unsigned int RECTANGLE_INDICES[] = {
-    0, 1, 3, // first triangle
-    1, 2, 3  // second triangle
-};
 float TRIANGLE_VERTICES[] = {
-    /*
-    //   x      y      z       r     g     b       texture S, texture T
-        -0.9F,  0.9F,  0.0F,   1.0F, 0.0F, 0.0F,   1.0F, 1.0F, // top left (red)
-        -0.7F,  0.9F,  0.0F,   0.0F, 1.0F, 0.0F,   0.0F, 1.0F, // top right (green)
-        -0.8F,  0.7F,  0.0F,   0.0F, 0.0F, 1.0F,   0.0F, 0.0F, // bottom middle (blue)
-    */
         -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
@@ -93,7 +72,6 @@ int SCR_WIDTH = 1280; // Window width
 int SCR_HEIGHT = 720; // Window height
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    (void)window; // Avoid unused parameter warning
     glViewport(0, 0, width, height);
 }
 
@@ -122,50 +100,6 @@ bool initGlad() {
         return false;
     }
     return true;
-}
-
-void setupVAO(unsigned int& VAO) {
-    glGenVertexArrays(1, &VAO); // Generate a VAO
-    glBindVertexArray(VAO);
-}
-
-void setupVBO(unsigned int& VBO, float* vertices, size_t size) {
-    // Generate a VBO
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(size), vertices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // color attribute
-    glVertexAttribPointer(1,
-                          3,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          8 * sizeof(float),
-                          (void*)(3 * sizeof(float))); // Disabled a warning on this line
-    glEnableVertexAttribArray(1);
-
-    // texture coordinate attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    // Do not unbind the VAO here when using EBO!
-    // The VAO unbinding should be done after EBO setup
-}
-
-void setupTriangleWithoutEBO(unsigned int& VBO) {
-    setupVBO(VBO, TRIANGLE_VERTICES, sizeof(TRIANGLE_VERTICES));
-
-    // Unbind VAO since we're not using EBO
-    glBindVertexArray(0);
-}
-
-void renderBackgroundColor(float red, float green, float blue, float alpha) {
-    glClearColor(red, green, blue, alpha);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color and depth buffers
 }
 
 unsigned int loadTexture(const char* path) {
@@ -198,171 +132,112 @@ unsigned int loadTexture(const char* path) {
 }
 
 int main() {
-    GLFWwindow* window = initWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL");
-    if (window == nullptr || !initGlad()) {
+    // --- Initialize GLFW and GLAD ---
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window =
+        glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL ECS", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD\n";
         return -1;
     }
 
-    // Setting up our main components
-    Shader ourShader("shaders/shader.vert.glsl", "shaders/shader.frag.glsl");
-    InputManager inputManager;
-    CameraSettings cameraSettings = {
-        glm::vec3(0.0f, 0.0f, 3.0f), // Position
-        glm::vec3(0.0f, 1.0f, 0.0f), // World Up
-        -90.0f,                      // Yaw
-        0.0f,                        // Pitch
-        45.0f                        // Fov
-    };
-    Camera camera(cameraSettings);
-    CameraController cameraController(camera);
-
-    // Initialize OpenGL options
-    int fbWidth;
-    int fbHeight;
-    glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
-    glViewport(0, 0, fbWidth, fbHeight);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glEnable(GL_DEPTH_TEST);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // Hide cursor
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Callbacks for input handling
-    glfwSetWindowUserPointer(window, &inputManager);
-    glfwSetKeyCallback(window, [](GLFWwindow* win, int key, int scancode, int action, int mods) {
-        auto* input = static_cast<InputManager*>(glfwGetWindowUserPointer(win));
-        if (!input)
-            return;
+    // --- Load textures ---
+    unsigned int tex1 = loadTexture("textures/container.png");
+    unsigned int tex2 = loadTexture("textures/emogi.png");
 
-        if (action == GLFW_PRESS) {
-            input->KeyPress(key);
-        } else if (action == GLFW_RELEASE) {
-            input->KeyRelease(key);
-        }
+    // --- Shader ---
+    Shader shader("shaders/shader.vert.glsl", "shaders/shader.frag.glsl");
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    // --- ECS registry ---
+    entt::registry registry;
+
+    // Camera entity
+    auto cameraEntity = registry.create();
+    registry.emplace<Camera>(cameraEntity);
+    registry.emplace<CameraController>(cameraEntity);
+
+    // Input entity
+    auto inputEntity = registry.create();
+    registry.emplace<Input>(inputEntity);
+
+    // --- Create cube mesh once ---
+    MeshRenderer cubeMesh =
+        MeshSystem::createCube(TRIANGLE_VERTICES, sizeof(TRIANGLE_VERTICES), tex1, tex2);
+
+    // --- Create cube entities ---
+    for (auto& pos : CUBE_POSITIONS) {
+        auto e = registry.create();
+        Transform tf;
+        tf.position = pos;
+        registry.emplace<Transform>(e, tf);
+        registry.emplace<MeshRenderer>(e, cubeMesh);
+    }
+
+    // --- GLFW callbacks for input ---
+    glfwSetWindowUserPointer(window, &registry);
+    glfwSetKeyCallback(window, [](GLFWwindow* win, int key, int sc, int action, int mods) {
+        auto* reg = static_cast<entt::registry*>(glfwGetWindowUserPointer(win));
+        auto& input = reg->get<Input>(reg->view<Input>().front());
+        input.keys[key] = (action == GLFW_PRESS || action == GLFW_REPEAT);
     });
     glfwSetCursorPosCallback(window, [](GLFWwindow* win, double xpos, double ypos) {
-        auto* input = static_cast<InputManager*>(glfwGetWindowUserPointer(win));
-        if (!input)
-            return;
-
-        input->MouseMove(static_cast<float>(xpos), static_cast<float>(ypos));
+        auto* reg = static_cast<entt::registry*>(glfwGetWindowUserPointer(win));
+        auto& input = reg->get<Input>(reg->view<Input>().front());
+        InputSystem::handleMouse(input, xpos, ypos);
     });
 
-    // Create separate VAOs for different objects
-    unsigned int triangleVAO;
-    unsigned int triangleVBO;
-
-    // Textures
-    unsigned int texture1 = loadTexture("textures/container.png");
-    if (texture1 == 0) {
-        std::cout << "Failed to load texture\n";
-        return -1;
-    }
-    unsigned int texture2 = loadTexture("textures/emogi.png");
-    if (texture2 == 0) {
-        std::cout << "Failed to load texture2\n";
-        return -1;
-    }
-
-    // Setup triangle (without EBO)
-    setupVAO(triangleVAO);
-    setupTriangleWithoutEBO(triangleVBO);
-
-    // Use our shader
-    ourShader.use(); // don't forget to activate the shader before setting uniforms!
-    glUniform1i(glGetUniformLocation(ourShader.getShaderProgramID(), "texture1"),
-                0);                  // set it manually
-    ourShader.setInt("texture2", 1); // or with shader class
-
-    // Coordinate Systems
-    glm::mat4 model = glm::mat4(1.0f); // Initialize to identity matrix
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-    glm::mat4 projection;
-    projection =
-        glm::perspective(glm::radians(cameraController.m_Camera.Fov),
-                         (float)SCR_WIDTH / (float)SCR_HEIGHT, // Cast to float for proper division
-                         0.1f,
-                         100.0f);
-
-    // Set transform matrices
-    GLint modelLoc = glGetUniformLocation(ourShader.getShaderProgramID(), "model");
-    GLint viewLoc = glGetUniformLocation(ourShader.getShaderProgramID(), "view");
-    GLint projLoc = glGetUniformLocation(ourShader.getShaderProgramID(), "projection");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-    // Main loop
-    float DELTATIME = 0.0f;     // Time between current frame and last frame
-    float LAST_FRAME = 0.0f;    // Time of last frame
-    float CURRENT_FRAME = 0.0f; // Current frame time
-
+    // --- Main loop ---
+    float lastFrame = 0.0f;
     while (glfwWindowShouldClose(window) == 0) {
-        // Per frame time logic
-        CURRENT_FRAME = glfwGetTime();
-        DELTATIME = CURRENT_FRAME - LAST_FRAME;
-        LAST_FRAME = CURRENT_FRAME;
+        float currentFrame = static_cast<float>(glfwGetTime());
+        float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
-        // Input
-        if (inputManager.IsKeyPressed(GLFW_KEY_W)) {
-            std::cout << "Moving forward\n";
-            cameraController.ProcessKeyboard(FORWARD, DELTATIME);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_S)) {
-            cameraController.ProcessKeyboard(BACKWARD, DELTATIME);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_A)) {
-            cameraController.ProcessKeyboard(LEFT, DELTATIME);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_D)) {
-            cameraController.ProcessKeyboard(RIGHT, DELTATIME);
-        }
-        cameraController.ProcessMouseMovement(inputManager.DeltaX, -inputManager.DeltaY);
-        inputManager.Update();
+        auto& input = registry.get<Input>(inputEntity);
+        auto& cam = registry.get<Camera>(cameraEntity);
+        auto& ctrl = registry.get<CameraController>(cameraEntity);
 
-        // Background color
-        renderBackgroundColor(0.2f, 0.3f, 0.3f, 1.0f); // Set background color
+        // --- Camera movement ---
+        if (input.keys[GLFW_KEY_W])
+            CameraSystem::processKeyboard(cam, ctrl, CameraMovement::FORWARD, deltaTime);
+        if (input.keys[GLFW_KEY_S])
+            CameraSystem::processKeyboard(cam, ctrl, CameraMovement::BACKWARD, deltaTime);
+        if (input.keys[GLFW_KEY_A])
+            CameraSystem::processKeyboard(cam, ctrl, CameraMovement::LEFT, deltaTime);
+        if (input.keys[GLFW_KEY_D])
+            CameraSystem::processKeyboard(cam, ctrl, CameraMovement::RIGHT, deltaTime);
 
-        // Bind texture
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
+        CameraSystem::processMouse(cam, ctrl, input.deltaX, -input.deltaY);
+        InputSystem::resetDeltas(input);
 
-        // Render triangle
-        glBindVertexArray(triangleVAO);
-        for (unsigned int i = 1; i < sizeof(CUBE_POSITIONS) / sizeof(CUBE_POSITIONS[0]); i++) {
-            glm::mat4 model = glm::mat4(1.0f); // Reset model matrix for each cube
-            model = glm::translate(model, CUBE_POSITIONS[i]);
-            float angle = 20.0f * i * (float)glfwGetTime(); // Rotate each cube at different speeds
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
-            ourShader.setMat4("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        // --- Render ---
+        RenderingSystem::renderScene(registry, shader, cam, SCR_WIDTH, SCR_HEIGHT);
 
-        // Rotate camera around the origin
-        view = glm::lookAt(cameraController.m_Camera.Position,
-                           cameraController.m_Camera.Position + cameraController.m_Camera.Front,
-                           cameraController.m_Camera.Up);
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-        projection = glm::perspective(glm::radians(cameraController.m_Camera.Fov),
-                                      (float)SCR_WIDTH /
-                                          (float)SCR_HEIGHT, // Cast to float for proper division
-                                      0.1f,
-                                      100.0f);
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-        // Swap buffers and poll events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Cleanup all resources
-    glDeleteVertexArrays(1, &triangleVAO);
-    glDeleteBuffers(1, &triangleVBO);
+    // Cleanup
+    glDeleteVertexArrays(1, &cubeMesh.VAO);
+    glDeleteBuffers(1, &cubeMesh.VBO);
     glfwTerminate();
     return 0;
 }
